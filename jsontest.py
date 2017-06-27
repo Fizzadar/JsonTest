2b024d1c3b4712a56a21b4bc3413953aecbe1a07
Nick Barrett <pointlessrambler@gmail.com>
2017-06-27 22:24:04 +0100

    Get files within the function so we see the name

--- jsontest.py
+++ jsontest.py
@@ -8,8 +8,9 @@ class JsonTest(type):
         files = listdir(attrs['jsontest_files'])
         files = [f for f in files if f.endswith('.json')]
 
-        def gen_test(test_name, test_data):
+        def gen_test(test_name, filename):
             def test(self):
+                test_data = json.loads(open(path.join(attrs['jsontest_files'], filename)).read())
                 self.jsontest_function(test_name, test_data)
 
             return test
@@ -17,10 +18,9 @@ class JsonTest(type):
         # Loop them and create class methods to call the jsontest_function
         for filename in files:
             test_name = filename[:-5]
-            test_data = json.loads(open(path.join(attrs['jsontest_files'], filename)).read())
 
             # Attach the method
             method_name = 'test_{0}'.format(test_name)
-            attrs[method_name] = gen_test(test_name, test_data)
+            attrs[method_name] = gen_test(test_name, filename)
 
         return type.__new__(cls, name, bases, attrs)
